21ac7b182045cc6c3f3b895839f09bf8171ecf50
George Leonidas <george.leonidas@atcom.gr>
2018-09-17 11:17:30 +0300

    Test project created

---
+++ Swift_Shadow_ImageView/Swift_Shadow_ImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Shadow_ImageView
+//
+//  Created by George Leonidas on 17/09/2018.
+//  Copyright © 2018 George Leonidas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
